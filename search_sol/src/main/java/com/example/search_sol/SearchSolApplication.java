7d763f3d05b497496a209a65adfa2caf75da274a
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-02 23:30:41 +0900

    initial setting

---
+++ search_sol/src/main/java/com/example/search_sol/SearchSolApplication.java
@@ -0,0 +1,13 @@
+package com.example.search_sol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSolApplication.class, args);
+    }
+
+}
---
+++ search_sol/src/test/java/com/example/search_sol/SearchSolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.search_sol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
