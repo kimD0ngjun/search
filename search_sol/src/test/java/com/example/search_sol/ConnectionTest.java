836125bf94f9a0960efadcc9339f503ab2a616f0
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-03 12:53:01 +0900

    test: 엘라스틱 서치 연결 테스트

--- search_sol/src/test/java/com/example/search_sol/ConnectionTest.java
+++ search_sol/src/test/java/com/example/search_sol/ConnectionTest.java
@@ -6,18 +6,13 @@ import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ConnectionTest {
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
     private RestClient restClient;
@@ -26,12 +21,5 @@ public class ConnectionTest {
     void elasticsearchShouldBeAvailable() throws IOException {
         Response response = restClient.performRequest(new Request("GET", "/"));
         assertEquals(200, response.getStatusLine().getStatusCode());
-    }
-
-    @Test
-    void kafkaShouldSendMessage() {
-        assertDoesNotThrow(() ->
-                kafkaTemplate.send("search", "Kafka 연결 테스트").get()
-        );
     }
 }
