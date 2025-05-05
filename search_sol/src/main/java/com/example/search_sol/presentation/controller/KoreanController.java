b28aac9a89584b8af22530e11aaf16a93ec38ca8
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-05 22:46:14 +0900

    feat: 기본 CRUD 관리자용 컨트롤러 작성

---
+++ search_sol/src/main/java/com/example/search_sol/presentation/controller/KoreanController.java
@@ -0,0 +1,48 @@
+package com.example.search_sol.presentation.controller;
+
+import com.example.search_sol.application.service.KoreanService;
+import com.example.search_sol.presentation.dto.KoreanCreateRequest;
+import com.example.search_sol.presentation.dto.KoreanResponse;
+import com.example.search_sol.presentation.dto.KoreanUpdateRequest;
+import com.example.search_sol.presentation.dto.SimpleKoreanUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/korean")
+@RequiredArgsConstructor
+public class KoreanController {
+
+    private final KoreanService koreanService;
+
+    @GetMapping
+    public ResponseEntity<KoreanResponse> getKorean(@RequestParam Long id) {
+        return ResponseEntity.ok(koreanService.getKorean(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<KoreanResponse> createKorean(@RequestBody KoreanCreateRequest request) {
+        return ResponseEntity.ok(koreanService.createKorean(request.toDTO()));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<KoreanResponse> updateKorean(
+            @RequestParam Long id,
+            @RequestBody KoreanUpdateRequest request) {
+        return ResponseEntity.ok(koreanService.updateKorean(id,request.toDTO()));
+    }
+
+    @PatchMapping("/simple-update")
+    public ResponseEntity<KoreanResponse> simpleUpdateKorean(
+            @RequestParam Long id,
+            @RequestBody SimpleKoreanUpdateRequest request
+    ) {
+        return ResponseEntity.ok(koreanService.simpleUpdateKorean(id, request.toDTO()));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<KoreanResponse> deleteKorean(@RequestParam Long id) {
+        return ResponseEntity.ok(koreanService.deleteKorean(id));
+    }
+}
