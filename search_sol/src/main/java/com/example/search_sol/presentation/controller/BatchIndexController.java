4aa68977079f0f397f81cc81fe10ba9c7db110ef
kimD0ngjun <chickentasty0112@gmail.com>
2025-05-07 12:04:42 +0900

    fix: 요청 매핑 URI 수정

--- search_sol/src/main/java/com/example/search_sol/presentation/controller/BatchIndexController.java
+++ search_sol/src/main/java/com/example/search_sol/presentation/controller/BatchIndexController.java
@@ -13,7 +13,7 @@ import javax.sql.DataSource;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/batch")
+@RequestMapping("/api/batch")
 @RequiredArgsConstructor
 public class BatchIndexController {
 
--- search_sol/src/main/java/com/example/search_sol/presentation/controller/KeywordSearchController.java
+++ search_sol/src/main/java/com/example/search_sol/presentation/controller/KeywordSearchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/search")
+@RequestMapping("/api/search")
 @RequiredArgsConstructor
 public class KeywordSearchController {
 
